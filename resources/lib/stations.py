146ac4cb260deff057f65dfb51d054fff1108dd9
nightflyer73 <nightflyer73@server.fake>
2014-08-06 10:18:54 +0200

    Force replay availability for Rai Gulp and Rai YoYo.

--- resources/lib/stations.py
+++ resources/lib/stations.py
@@ -5,7 +5,18 @@ import StorageServer
 def get_tv_stations():
     url = "http://www.rai.tv/dl/RaiTV/iphone/android/smartphone/advertising_config.html"
     response = json.load(urllib2.urlopen(url))
-    return response["Channels"]
+    channels = response["Channels"]
+    
+    for channel in channels:
+        # Force replay availability for Rai Gulp and Rai YoYo
+        if channel["name"] in ("Rai Gulp", "Rai Yoyo"):
+            channel["hasReplay"] = "YES"
+            
+        # Fix tag for Rai Gulp
+        if channel["name"] == "Rai Gulp":
+            channel["tag"] = "RaiGulp"
+    
+    return channels
     
 def get_radio_station():
     url = "http://www.rai.tv/dl/RadioRai/config_json.html"
