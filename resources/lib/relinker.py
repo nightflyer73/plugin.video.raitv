d0a5a9b94c8b2f77d5bc2fc7fc1a1e25f818b5a7
nightflyer73 <nightflyer73@server.fake>
2018-02-28 15:36:42 +0100

    Updated user agent strings.

--- resources/lib/relinker.py
+++ resources/lib/relinker.py
@@ -3,18 +3,16 @@ import urllib2
 import urlparse
 
 class Relinker:
-    # Rai.tv android app
-    # UserAgent = "Apache-HttpClient/UNAVAILABLE (java 1.4)"
-    # Firefox 29 on Android
-    # UserAgent = "Mozilla/5.0 (Android; Mobile; rv:29.0) Gecko/29.0 Firefox/29.0"
-    # Firefox 29 on Windows 7
-    # UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0"
-    # Firefox 29 on Linux
-    # UserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0"
+    # Firefox 52 on Android
+    # UserAgent = "Mozilla/5.0 (Android; Mobile; rv:52.0) Gecko/52.0 Firefox/52.0"
+    # Firefox 52 ESR on Windows 7
+    # UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0"
+    # Firefox 52 ESR on Linux
+    # UserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
+    # Chrome 64 on Windows 10
+    # UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282 Safari/537.36"
     # Raiplay android app
-    # UserAgent = "Android 4.2.2 (smart) / RaiPlay 2.0.4 / WiFi"
-    # Chrome 57 on Windows 10
-    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2956.0 Safari/537.36"
+    UserAgent = "Android 4.2.2 (smart) / RaiPlay 2.1.3 / WiFi"
     
     def __init__(self):
         opener = urllib2.build_opener()
